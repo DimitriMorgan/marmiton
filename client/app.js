38bd6ef9d629bef157dc71acb53c6ab71e53e4d6
Michelle Chua <chuanarra.michelle@gmail.com>
2015-06-27 20:18:47 +0200

    move rootctrl to homeCtrl in controllers folder

--- client/app.js
+++ client/app.js
@@ -2,11 +2,6 @@
 
 var App = angular.module('MarmitonApp', []);
 
-App.controller('root', function($scope, GlobalFunctions) {
-    $scope.firstName= "John";
-    $scope.lastName= "Doe";
-});
-
 App.factory('GlobalFunctions', function() {
    return {
        getRecipes: function(type) {
