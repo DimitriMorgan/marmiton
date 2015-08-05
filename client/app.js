5e2f0558204463730c91209adf833d213065591b
Michelle Chua <chuanarra.michelle@gmail.com>
2015-08-05 10:42:35 +0200

    push all

--- client/app.js
+++ client/app.js
@@ -1,21 +1,31 @@
 'use strict';
 
-var App = angular.module('MarmitonApp', ['ngRoute', "com.2fdevs.videogular", "youtube-embed"]);
+var App = angular.module('MarmitonApp', [
+    "ngRoute",
+    "com.2fdevs.videogular",
+    "youtube-embed",
+    "ngAnimate",
+    "ngTouch"
+]);
 
 App.config(['$routeProvider',
     function($routeProvider) {
-        $routeProvider.
-            when('/home', {
-                templateUrl: 'client/app/views/home.html',
-                controller: 'HomeCtrl'
-            }).
-            when('/recipe/:recipeId', {
-                templateUrl: 'client/app/views/profile.html',
-                controller: 'ProfileCtrl'
-            }).
-            otherwise({
-                redirectTo: '/home'
-            });
+        $routeProvider
+          .when('/home', {
+            templateUrl: 'client/app/views/home.html',
+            controller: 'HomeCtrl'
+          })
+          .when('/recipe/:recipeId', {
+            templateUrl: 'client/app/views/profile.html',
+            controller: 'ProfileCtrl'
+          })
+          .when('/sliderPage', {
+            templateUrl: 'client/app/views/slider.html',
+            controller: 'SliderCtrl'
+          })
+          .otherwise({
+            redirectTo: '/home'
+          });
     }]);
 
 App.factory('GlobalFunctions', function() {
