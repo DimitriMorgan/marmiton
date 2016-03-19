30ad750885ec3b6223988b678c143730db6a6bfe
DimitriMorgan <dim.fruit@gmail.com>
2016-03-19 20:17:12 +0100

    add id on load all and insert steps

--- index.php
+++ index.php
@@ -5,5 +5,11 @@
     <input name="title" value="title-test">
     <input name="dish_type" value="dish-type-test">
     <input name="tags" value="tags-test">
+    <input name="step_1_title" value="titre etape 1">
+    <input name="step_1_description" value="description etape 1">
+    <input name="step_2_title" value="titre etape 2">
+    <input name="step_2_description" value="description etape 2">
+    <input name="step_3_title" value="titre etape 3">
+    <input name="step_3_description" value="description etape 3">
     <input type="submit">
 </form>
